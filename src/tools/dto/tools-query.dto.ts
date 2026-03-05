12fb30730e20a98dc094bf859d90d4ebb3964653
Peter Francois <peter.francois237@hotmail.com>
2026-03-05 16:54:58 +0100

    feat: add class validator

--- src/tools/dto/tools-query.dto.ts
+++ src/tools/dto/tools-query.dto.ts
@@ -1,9 +1,26 @@
+import { IsEnum, IsNumber, IsOptional, IsString, Min } from "class-validator";
 import { ToolsOwnerDepartment, ToolsStatus } from "../entities/tool.entity.js";
 
 export class ToolsQueryDto {
+  @IsOptional()
+  @IsEnum(ToolsOwnerDepartment)
   department?: ToolsOwnerDepartment;
+
+  @IsOptional()
+  @IsEnum(ToolsStatus)
   status?: ToolsStatus;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
   min_cost?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
   max_cost?: number;
+
+  @IsOptional()
+  @IsString()
   category?: string;
 }
