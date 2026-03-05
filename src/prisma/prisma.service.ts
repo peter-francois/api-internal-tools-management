c9bdfe8aaca4e0dbad92afc126eac9f553fa00e9
peter-francois <contact@peterfrancois.dev>
2026-03-05 11:38:43 +0100

    chore: add prisma ORM and initial configuration

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '../generated/prisma/client.js';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor() {
+    const adapter = new PrismaMariaDb({
+      database: process.env.DATABASE_URL as string,
+    });
+    super({ adapter });
+  }
+}
