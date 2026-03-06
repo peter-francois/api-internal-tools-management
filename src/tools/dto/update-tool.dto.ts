f4a12bb7ff9f736b58b208d7f73ca1c6aa434050
Peter Francois <peter.francois237@hotmail.com>
2026-03-06 15:18:49 +0100

    feat: create update tool function

--- src/tools/dto/update-tool.dto.ts
+++ src/tools/dto/update-tool.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from "@nestjs/mapped-types";
+import { PartialType } from "@nestjs/swagger";
 import { CreateToolDto } from "./create-tool.dto.js";
 
 export class UpdateToolDto extends PartialType(CreateToolDto) {}
