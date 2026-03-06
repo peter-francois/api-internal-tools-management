251b487bba52fa4a2c5975cecbc877b6136aaa8b
Peter Francois <peter.francois237@hotmail.com>
2026-03-06 15:52:50 +0100

    refactor: move examples from utils to tools

--- src/tools/tools.controller.ts
+++ src/tools/tools.controller.ts
@@ -26,7 +26,7 @@ import {
   TOOLS_CREATE_OR_UPDATE_EXAMPLE,
   TOOLS_FIND_ALL_EXAMPLE,
   TOOLS_UPDATE_BODY_EXAMPLE,
-} from "../utils/tools.example.js";
+} from "./tools.example.js";
 
 @ApiTags("tools")
 @Controller("tools")
