769908c7562f3b09f6d992f26dabc4c682971a3b
Peter Francois <peter.francois237@hotmail.com>
2026-03-06 10:45:42 +0100

    docs: add Swagger documentation for findAll endpoint

---
+++ src/utils/success-response.factory.ts
@@ -0,0 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export function SuccessResponseFactory<T, M>(DataClass: new () => T, MetaClass: new () => M) {
+  class SuccessResponseClass {
+    @ApiProperty({ type: () => DataClass, isArray: true })
+    data: T[];
+
+    @ApiProperty({ type: () => MetaClass })
+    meta: M;
+  }
+  return SuccessResponseClass;
+}
