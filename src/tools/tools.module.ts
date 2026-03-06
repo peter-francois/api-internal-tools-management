e2b5c55d9d7707c701532b2bcc1eb365ae8b4763
peter-francois <contact@peterfrancois.dev>
2026-03-06 17:03:44 +0100

    refactor: extract repository layer to decouple Prisma from service

--- src/tools/tools.module.ts
+++ src/tools/tools.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { ToolsController } from "./tools.controller.js";
 import { ToolsService } from "./tools.service.js";
 import { PrismaService } from "../prisma/prisma.service.js";
+import { ToolsRepository } from "./tools.repository.js";
 
 @Module({
   controllers: [ToolsController],
-  providers: [ToolsService, PrismaService],
+  providers: [ToolsService, PrismaService, ToolsRepository],
 })
 export class ToolsModule {}
