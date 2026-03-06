be3e8ecc2f287464355620795a1a3f3dc5c77b12
peter-francois <contact@peterfrancois.dev>
2026-03-06 10:02:11 +0100

    feat: create SuccessResponseInterface and use it in tools service

--- src/utils/response.interface.ts
+++ src/utils/response.interface.ts
@@ -2,3 +2,8 @@ export interface ErrorResponseInterface<T> {
   error: string;
   details: T;
 }
+
+export interface SuccessResponseInterface<T, M = Record<string, unknown>> {
+  data: T;
+  meta?: M;
+}
